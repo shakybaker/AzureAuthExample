d779a24408fc8e5b4a6193af5d60f6d829271702
Mark Baker <shakybaker@gmail.com>
2013-12-06 11:58:15 +0000

    Initial

---
+++ ContactManagerExample/ContactManagerExample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ContactManagerExample.Startup))]
+namespace ContactManagerExample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
